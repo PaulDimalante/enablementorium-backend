44297dba23d988a1a484efb5d2ee2785cf9c6400
Will Schipp <wschipp@corelogic.com>
2018-12-31 15:48:05 -0600

    increased coverage and cleaned up tech debt

---
+++ src/test/java/com/cognizant/labs/model/listener/PersonListenerTest.java
@@ -0,0 +1,30 @@
+package com.cognizant.labs.model.listener;
+
+import com.cognizant.labs.model.Person;
+import com.cognizant.labs.service.PublishingService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.support.membermodification.MemberModifier;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(PowerMockRunner.class)
+public class PersonListenerTest {
+
+    @Test
+    public void testInvoked() throws Exception {
+        //instantiate
+        PersonListener listener = new PersonListener();
+        Person person = new Person();
+        //mock
+        PublishingService service = mock(PublishingService.class);
+        MemberModifier.field(PersonListener.class,"publishingService").set(listener,service);
+        //execute
+        listener.postUpdate(person);
+        //verify invoked
+        verify(service,atLeastOnce()).sendUpdate(any());
+    }
+
+}
