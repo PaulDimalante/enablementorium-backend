e85912bfa64137cc402365670537d9090cdeab65
Will Schipp <wschipp@corelogic.com>
2019-02-04 09:41:28 -0600

    oauth2 pass through complete

--- src/main/java/com/cognizant/labs/Application.java
+++ src/main/java/com/cognizant/labs/Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 public class Application {
@@ -27,7 +27,15 @@ public class Application {
 
     @Profile("default")
     @EnableGlobalMethodSecurity(prePostEnabled = true)
-    @EnableResourceServer
     @Configuration
-    class SecurityConfig { }
+    static class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .oauth2ResourceServer().jwt();
+        }
+    }
 }
