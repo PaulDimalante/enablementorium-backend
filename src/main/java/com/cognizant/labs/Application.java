8b6b8cfff0cb30ce38f667e3787a0f21bdee8114
Will Schipp <wschipp@corelogic.com>
2019-02-06 14:28:02 -0600

    added custom evalutor

--- src/main/java/com/cognizant/labs/Application.java
+++ src/main/java/com/cognizant/labs/Application.java
@@ -1,13 +1,18 @@
 package com.cognizant.labs;
 
 import com.cognizant.labs.security.EncryptionUtil;
+import com.cognizant.labs.security.KeyCloakOAuthPermissionEvaluator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -55,9 +60,8 @@ public class Application {
     }
 
     @Profile("default")
-    @EnableGlobalMethodSecurity(prePostEnabled = true)
     @Configuration
-    static class SecurityConfig extends WebSecurityConfigurerAdapter {
+    class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
@@ -65,6 +69,25 @@ public class Application {
                     .anyRequest().authenticated()
                     .and()
                     .oauth2ResourceServer().jwt();
+        }
+    }
+
+    @Profile("default")
+    @Bean
+    public PermissionEvaluator permissionEvaluator() {
+        return new KeyCloakOAuthPermissionEvaluator();
+    }
+
+    @Profile("default")
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    @Configuration
+    class EvaluatorConfig extends GlobalMethodSecurityConfiguration {
+
+        @Override
+        protected MethodSecurityExpressionHandler createExpressionHandler() {
+            DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+            expressionHandler.setPermissionEvaluator(permissionEvaluator());
+            return expressionHandler;
         }
     }
 }
