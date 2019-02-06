70d6f0e1c645e419b17e3492902516715a53f1c4
Will Schipp <wschipp@corelogic.com>
2019-02-06 09:34:35 -0600

    test coverage

--- src/test/java/com/cognizant/labs/ApplicationIT.java
+++ src/test/java/com/cognizant/labs/ApplicationIT.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -21,4 +22,8 @@ public class ApplicationIT {
         assertTrue(context.getBeanDefinitionCount() > 0);
     }
 
+    @Test
+    public void testEncryptionBean() {
+        assertNotNull(new Application().encryptionUtil());
+    }
 }
